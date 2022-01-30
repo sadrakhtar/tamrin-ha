8de62e2d95f8fa4a994dd00d37e694e6d08b978c
SadraAkhtarshnas <95992595+sadrakhtar@users.noreply.github.com>
2022-01-30 10:34:05 -0800

    Add files via upload

---
+++ tamrin11.py
@@ -0,0 +1,15 @@
+b = "Hello, World!"
+print(b[2:5])
+
+
+
+b = "Hello, World!"
+print(b[:5])
+
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
---
+++ yamrin13.py
@@ -0,0 +1,11 @@
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
